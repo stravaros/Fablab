7c012c6bc819c83b9655cf4e0cd797b872dccbe0
Vincent Abikhattar <Vincent.Abikhattar@ensimag.grenoble-inp.fr>
2014-04-16 13:55:40 +0200

    new gui

--- Version2/client/src/View/Frame.java
+++ Version2/client/src/View/Frame.java
@@ -1,12 +1,19 @@
 package View;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLException;
+import javax.media.opengl.glu.GLU;
 import javax.media.opengl.glu.GLUquadric;
 import javax.media.opengl.glu.gl2.GLUgl2;
 
 import com.jogamp.opengl.util.gl2.GLUT;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
 
 import capteur.Capteur;
 import Model.Mdl;
@@ -20,12 +27,15 @@ public class Frame implements GLEventListener {
 	double longueur;
 	double largeur;
 	float taille = (float) 0.5;
+	Texture identifiant_texture;
+	File bois = new File("ressources/textures/bois.jpg") ;
 
 	public Frame(GLAutoDrawable gld, Mdl m) {
 		this.drawable = gld;
 		this.mdl = m;
 		this.longueur = 20;
 		this.largeur = 20;
+		
 	}
 
 	// IMPLEMENTE ELENVENTLISTENER
@@ -35,6 +45,15 @@ public class Frame implements GLEventListener {
 		final GL2 gl = drawable.getGL().getGL2();
 		GLUgl2 glu = new GLUgl2(); //INSTANCIATION GLU
 		GLUT glut = new GLUT();	//INSTANCIATION GLUT
+		
+		try {
+			identifiant_texture = TextureIO.newTexture(bois, true);
+			identifiant_texture.enable(gl);
+			identifiant_texture.bind(gl);
+		} catch (GLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
 		
 		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);  
 		gl.glMatrixMode(GL2.GL_MODELVIEW);
@@ -99,11 +118,15 @@ public class Frame implements GLEventListener {
 	//	float mat_diffuse[] = {0.0f,255.0f,0.0f,1.0f};
 		//gl.glMaterialfv(GL2.GL_FRONT,  GL2.GL_DIFFUSE, mat_diffuse, 0);
 		gl.glBegin(GL2.GL_QUADS);
-		gl.glColor3d(0, 0, 1); // set the color of the quad
-			gl.glVertex3d(-longueur+1, largeur, 0); // Top Left
-			gl.glVertex3d(longueur, largeur, 0); // Top Right
-			gl.glVertex3d(longueur, -largeur+1, 0); // Bottom Right
-			gl.glVertex3d(-longueur+1, -largeur+1, 0); // Bottom Left
+		gl.glColor3d(1, 1, 1); // set the color of the quad
+			gl.glTexCoord2d(0, 0); 		gl.glVertex3d(-longueur, -largeur, 0); // Top Left
+			gl.glTexCoord2d(1, 0);		gl.glVertex3d(longueur, -largeur, 0); // Top Right
+			gl.glTexCoord2d(1, 1); 	gl.glVertex3d(longueur, largeur, 0); // Bottom Right
+			gl.glTexCoord2d(0, 1);  	gl.glVertex3d(-longueur, largeur, 0); // Bottom Left
+			    // glVertex3i(-10,-10,-1);
+			   // glVertex3i(10,-10,-1);
+			  // glVertex3i(10,10,-1);
+			   //glVertex3i(-10,10,-1);
 		gl.glEnd();
 	}
 	
@@ -243,6 +266,7 @@ public class Frame implements GLEventListener {
 		gl.glMatrixMode(GL2.GL_MODELVIEW);
 		gl.glLoadIdentity();
 		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		gl.glEnable(GL2.GL_TEXTURE_2D);
 		/*float mat_ambient[] = {0.2f,0.2f,0.2f,0.2f};
 		float mat_diffuse[] = {1.0f,1.0f,1.0f,1.0f};
 		float mat_specular[] = {1.0f,1.0f,1.0f,1.0f};
